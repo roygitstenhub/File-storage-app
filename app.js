40f480a101f27361e1e72ef036fd014da76c2af4
Roysten <roystongonsalves2002@gmail.com>
2025-11-27 20:05:22 +0530

    added custom ci/cd endpoint

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ import limiter from "./services/rateLimiter.js"
 // import throttle from "./services/throttling.js"
 import subscriptionRoutes from "./routes/subscriptionRoutes.js"
 import webhookRoutes from "./routes/webhookRoutes.js"
+import { spawn } from "child_process"
+
 
 const PORT = process.env.PORT || 3000
 
@@ -23,30 +25,61 @@ const app = express()
 app.use(cookieParser(process.env.SESSION_SECRET))
 app.use(express.json())
 
-var whitelist = [process.env.FRONTEND_ORIGIN_1,process.env.FRONTEND_ORIGIN_2]
+var whitelist = [process.env.FRONTEND_ORIGIN_1, process.env.FRONTEND_ORIGIN_2]
 
 app.use(cors({
-    origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin ) {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
     }
   },
-    credentials: true
+  credentials: true
 }))
 
 app.use(helmet())
 
 app.use(limiter)
-app.get("/",(req,res)=>{
-	res.json({message:"hello from storageapp"})
+
+app.post("/github-webhook", (req, res) => {
+  console.log(req.body)
+  console.log(req.headers)
+  const bashChildProcess = spawn("bash", ["/home/ubuntu/deployfrontend.sh"])
+
+  bashChildProcess.stdout.on("data", (data) => {
+    process.stdout.write(data)
+  })
+
+  bashChildProcess.stderr.on("data", (data) => {
+    process.stdout.write(data)
+  })
+
+  bashChildProcess.on("close", (code) => {
+    res.json({message:"OK"})
+    if (code === 0) {
+      console.log("script executed successfully")
+    } else {
+      console.log("script failed")
+    }
+  })
+
+  bashChildProcess.on("error", (err) => {
+    res.json({message:"OK"})
+    console.log("Error in spawning the  process")
+    console.log(err.message)
+  })
+
 })
 
-app.get("/",(req,res)=>{
-    res.json({
-        message:"Hello from storageApp"
-    })
+app.get("/", (req, res) => {
+  res.json({ message: "hello from storageapp" })
+})
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from storageApp"
+  })
 })
 
 app.use("/directory", checkAuth, directoryRoutes)
@@ -57,14 +90,14 @@ app.use("/subscriptions", checkAuth, subscriptionRoutes)
 app.use("/webhooks", webhookRoutes)
 
 app.use((err, req, res, next) => {
-    console.log(err)
-    res.status(err.status || 500).json({
-        error: "Something went wrong"
-    })
+  console.log(err)
+  res.status(err.status || 500).json({
+    error: "Something went wrong"
+  })
 })
 
 
 app.listen(PORT, () => {
-    console.log(`server running at ${PORT}`)
+  console.log(`server running at ${PORT}`)
 })
 
