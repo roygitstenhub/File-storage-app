3aa738f987fad481286ffb045f77255f7ff614f1
Roysten <roystongonsalves2002@gmail.com>
2025-11-28 14:56:50 +0530

    changed app.js file

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3000
 
 await connectDb()
 const app = express()
-app.set("trust proxy", true)
+app.set("trust proxy",1)
 app.use(cookieParser(process.env.SESSION_SECRET))
 app.use(express.json())
 
