6cdc3094a291adaaff8ea842a37638511902218c
Roysten <roystongonsalves2002@gmail.com>
2025-12-03 12:01:45 +0530

    changed the home route

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.get("/", (req, res) => {
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello from storageApp! Admin : Roysten"
+    message: "Hello from storageApp! [ App Admin : Roysten Gonsalves ]"
   })
 })
 
